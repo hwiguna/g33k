f75c413f80c9af6290d759605a27626e621a3a9c
Hari Wiguna <hwiguna@gmail.com>
2014-03-09 23:43:45 -0500

    More analog pins

--- ArduinoProjects/Utilities/AnalogIn/AnalogIn.ino
+++ ArduinoProjects/Utilities/AnalogIn/AnalogIn.ino
@@ -1,4 +1,6 @@
-const int analogInPin = A5;  // Analog input pin that the potentiometer is attached to
+const int xSensor = A5;  // Analog input pin that the potentiometer is attached to
+const int ySensor= A4;  // Analog input pin that the potentiometer is attached to
+const int zSensor = A3;  // Analog input pin that the potentiometer is attached to
 
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
@@ -9,16 +11,21 @@ void setup() {
 }
 
 void loop() {
-  // read the analog in value:
-  sensorValue = analogRead(analogInPin);            
-  // map it to the range of the analog out:
+    int xValue = analogRead(xSensor);
+    int yValue = analogRead(ySensor);
+    int zValue = analogRead(zSensor);
+    
+    Serial.print(xValue); Serial.print(", "); 
+    Serial.print(yValue); Serial.print(", "); 
+    Serial.print(zValue); Serial.println(); 
+
   outputValue = map(sensorValue, 0, 1023, 0, 255);  
 
   // print the results to the serial monitor:
-  Serial.print("sensor = " );                       
-  Serial.print(sensorValue);      
-  Serial.print("\t output = ");      
-  Serial.println(outputValue);   
+//  Serial.print("sensor = " );                       
+//  Serial.print(sensorValue);      
+//  Serial.print("\t output = ");      
+//  Serial.println(outputValue);   
 
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
