e559ec88908d4ba2eea782a028095506dd0abaa8
Hari Wiguna <hwiguna@gmail.com>
2014-07-26 07:55:39 -0500

    Added three touch sensors and two potentiometers

---
+++ ArduinoProjects/_8x8x8_lue_LED_cube12/_8x8x8_lue_LED_cube12.ino
@@ -0,0 +1,469 @@
+#include <CapacitiveSensor.h>
+
+// 8x8x8x Blue LED Cube
+// by Hari Wiguna 2014
+//
+// v03 - blinks all 512
+// v04 - more test patterns.  Uses TimerOne library.
+// v06 - Using MsTimer2
+// v07 - Don't turn off previous layer until the very last moment for brighter display
+// v09 - Pedal to the metal, no timer libraries!
+// v10 - Invert shift register output in DrawLayers() because we longer use transistors there.
+// v11 - Add Capsense library
+// v12 - Add two potentiometers on Analog 4 and 5
+
+//-- Shift Register pins --
+int latchPin = 13; // Arduino D13 to IC pin 12 (ST_CP) -- White
+int clockPin = 12; // Arduino D12 to IC pin 11 (SH_CP) -- Yelow
+int dataPin  = 11; // Arduino D11 to IC pin 14 (DS) -- Blue
+
+//-- Globals --
+volatile int8_t cube[8][8]; // byte bits = X, 1st index=Y, 2nd index = Z
+volatile int8_t gZ = 0;
+int pot0; // Left Potentiometer (A4)
+int pot1; // Right Potentiometer (A5)
+int animSpeed; // Animation speed controlled by pot0
+
+//-- CapSense --
+CapacitiveSensor   cs_14_15 = CapacitiveSensor(14,15);
+CapacitiveSensor   cs_14_16 = CapacitiveSensor(14,16);
+CapacitiveSensor   cs_14_17 = CapacitiveSensor(14,17);
+
+void SetupPins()
+{
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+
+  for (int i=0; i<8; i++) {
+    pinMode(2+i, OUTPUT);
+  }
+}
+
+void SetupCapSense()
+{
+  pinMode(14+0,OUTPUT);
+  pinMode(14+1,OUTPUT); //15
+  pinMode(14+2,OUTPUT); //16
+  pinMode(14+3,OUTPUT); //17
+  pinMode(A4,INPUT);
+  pinMode(A5,INPUT);
+  //cs_14_15.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+}
+
+void setup(void) {
+  SetupPins();
+  SetupCapSense();
+  CubeAllOff();
+  //RunTests();
+  SetupTimer();
+  Serial.begin(9600);
+}
+
+void SetupTimer()
+{
+  cli();
+  // Reset any PWM that arduino may have setup automatically
+  TCCR2A = 0;
+  TCCR2B = 0;
+  
+  TCCR2A |= (1 << WGM21); // CTC mode. Reset counter when OCR2 is reached
+
+  TCCR2B |= (1 << CS21) | (1 << CS22); // Prescaler = 256
+  //TCCR2B |= (1 << CS20) | (1 << CS22); // Prescaler = 128
+  OCR2A = 70; // Fire interrupt when timer2 has looped 80 times
+
+  TCNT2 = 0; // initial counter value = 0;
+  TIMSK2 |= (1<<OCIE2A); // Enable CTC interrupt
+  sei();
+}
+
+ISR (TIMER2_COMPA_vect)
+{
+  Refresh();
+}
+
+void RunTests()
+{
+  Serial.begin(9600);
+
+  Serial.print("TestCubeAllOff = ");
+  Serial.println(TestCubeAllOff());
+
+  Serial.print("TestRefresh = ");
+  Serial.println(TestRefresh());    // 8052 micro seconds
+
+  Serial.end();
+}
+
+long TestCubeAllOff()
+{
+  long start = micros();
+  CubeAllOff();
+  long finish = micros();
+  return finish-start;
+}
+
+long TestRefresh()
+{
+  long start = micros();
+  Refresh();
+  long finish = micros();
+  return finish-start;
+}
+
+void TestPattern3()
+{
+  CubeAllOff();
+  delay(500);
+  CubeAllOn();
+  delay(500);
+}
+
+
+void BottomUp()
+{
+  for (int8_t z=0; z<8; z++) {
+    for (int8_t x=0; x<8; x++) {
+      for (int8_t y=0; y<8; y++) {
+        SetDot(x,y,z); 
+      }
+    }
+    delay(64);
+    CubeAllOff();
+  }
+}
+
+
+void OneWall()
+{
+  for (int8_t z=0; z<8; z++) {
+    for (int8_t x=0; x<8; x++) {
+      for (int8_t y=7; y<8; y++) {
+        SetDot(x,y,z); 
+      }
+    }
+  }
+}
+
+void LeftRight()
+{
+  for (int8_t x=0; x<8; x++) {
+    for (int8_t z=0; z<8; z++) {
+      for (int8_t y=7; y<8; y++) {
+        SetDot(x,y,z); 
+      }
+    }
+    delay(64);
+    CubeAllOff();
+  }
+}
+
+void Refresh(void) // WITHOUT the added delayMicroseconds, this routine takes 8052 microseconds
+{
+  noInterrupts();
+
+  //-- Compute new layer --
+  int8_t prevLayer = gZ;
+  gZ++; 
+  if (gZ>=8) gZ=0;
+
+  // Prepare for data. Shift data to shift registers but do not reflect it on the outputs yet.
+  digitalWrite(latchPin, LOW);
+
+  //-- Spit out the bits --
+  DrawLayer(gZ);
+
+  //-- Turn off previous layer --
+  digitalWrite(2+prevLayer,LOW); // Turn off prev layer
+
+  //-- Turn on this layer --
+  digitalWrite(2+gZ,HIGH); // Turn on this layer
+
+
+  // All data ready. Instantly reflect all 64 bits on all 8 shift registers to the led layer.
+  digitalWrite(latchPin, HIGH);
+
+  interrupts();
+}
+
+void TurnOnLayer(int8_t z)
+{
+  int8_t prev = z==0 ? 7 : z-1;
+
+  // Prepare for data. Shift data to shift registers but do not reflect it on the outputs yet.
+  digitalWrite(latchPin, LOW);
+
+  //-- Spit out the bits --
+  DrawLayer(z);
+
+  //-- Turn off previous layer --
+  digitalWrite(2+prev,LOW); // Turn off prev layer
+}
+
+void TurnOffLayer(int8_t z)
+{
+  // All data ready. Instantly reflect all 64 bits on all 8 shift registers to the led layer.
+  digitalWrite(latchPin, HIGH);
+
+  //-- Turn on this layer --
+  digitalWrite(2+z,HIGH); // Turn on this layer
+}
+
+void DrawLayer(int8_t z)
+{
+  // Spit out all 64 bits for the layer.
+  for (int8_t y=0; y<8; y++) {
+    shiftOut(dataPin, clockPin, MSBFIRST, ~cube[y][z]); // Push Most significant BYTE first   
+  }  
+}
+
+void LayerOn(int8_t z)
+{
+  for (int8_t y=0; y<8; y++) {
+    for (int8_t x=0; x<8; x++) {
+      SetDot(x,y,z);
+    }  
+  }  
+}
+
+void CubeAllOn()
+{
+  //noInterrupts();
+  for (int8_t z=0; z<8; z++) {
+    for (int8_t y=0; y<8; y++) {
+      for (int8_t x=0; x<8; x++) {
+        SetDot(x,y,z);
+      }  
+    }  
+  }  
+  //interrupts();
+}
+
+void CubeAllOff()
+{
+  for (int8_t z=0; z<8; z++) {
+    SetLayer(z, 0x00);
+  }  
+}
+
+void CubeUp()
+{
+  for (int8_t z=0; z<8; z++) {
+    SetLayer(z, 0xFF);
+    delay(animSpeed);
+    SetLayer(z, 0x00);
+  }  
+}
+
+void CubeDown()
+{
+  for (int8_t z=7; z>=0; z--) {
+    SetLayer(z, 0xFF);
+    delay(animSpeed);
+    SetLayer(z, 0x00);
+  }  
+}
+
+void CubeLeftRight()
+{
+  for (int8_t x=0; x<8; x++) {
+    SetXPlane(x);
+    delay(animSpeed);
+    CubeAllOff();
+  }  
+}
+
+void CubeRightLeft()
+{
+  for (int8_t x=7; x>=0; x--) {
+    SetXPlane(x);
+    delay(animSpeed);
+    CubeAllOff();
+  }  
+}
+
+void SetXPlane(int8_t x)
+{
+  x = Wrap(x);
+  int8_t xPattern = 1 << x;
+  for (int8_t z=0; z<8; z++) {
+    for (int8_t y=0; y<8; y++) {
+      cube[y][z] = xPattern;
+    }
+  }
+}
+
+void TestPattern1()
+{
+  //int8_t y = 7;
+  for (int8_t x=0; x<8; x++) {
+    for (int8_t y=0; y<8; y++) {
+      SetDot(x,y,0); 
+      SetDot(7-x,y,1);
+    }
+
+    delay(20);
+
+    for (int8_t y=0; y<8; y++) {
+      ClearDot(x,y,0); 
+      ClearDot(7-x,y,1);
+    }
+  }
+}
+
+void TestPattern2_Scan_one_layer(int8_t z)
+{
+  for (int8_t y=0; y<8; y++) {
+    for (int8_t x=0; x<8; x++) {
+      SetDot(x,y,z);
+      delay(300);
+      ClearDot(x,y,z);
+    }
+  }
+}
+
+void One_Pixel_Up_a_wall(int8_t y)
+{
+  for (int8_t z=0; z<8; z++) {
+    for (int8_t x=7; x>=0; x--) {
+      SetDot(x,y,z);
+      delay(64);
+      ClearDot(x,y,z);
+    }
+  }
+}
+
+void Line_Up_a_wall(int8_t y)
+{
+  for (int8_t z=0; z<8; z++) {
+    for (int8_t x=7; x>=0; x--) {
+      SetDot(x,y,z);
+    }
+    delay(64);
+    CubeAllOff();
+  }
+}
+
+
+void TestPattern_scan_all_layers()
+{
+  for (int8_t z=0; z<8; z++) {
+    for (int8_t y=0; y<8; y++) {
+      for (int8_t x=0; x<8; x++) {
+        SetDot(x,y,z);
+        delay(64);
+        ClearDot(x,y,z);
+      }
+    }
+  }
+}
+
+void TestPattern4_Scan_one_wall()
+{
+  for (int8_t z=0; z<8; z++) {
+    for (int8_t x=7; x>=0; x--) {
+      SetDot(x,7,z);
+      delay(64);
+      ClearDot(x,7,z);
+    }
+  }
+}
+
+void TestPattern5_swipe_wall_up()
+{
+  for (int8_t z=0; z<8; z++) {
+    SetLayer(z,0xFF);
+    //for (int8_t x=0; x<8; x++) SetDot(x,7,z);
+    //for (int8_t y=3; y<8; y++) for (int8_t x=0; x<8; x++) SetDot(x,y,z);
+    delay(64);
+    //SetLayer(z,0x00);
+    CubeAllOff();
+    //for (int8_t x=0; x<8; x++) ClearDot(x,7,z);
+    //for (int8_t y=3; y<8; y++) for (int8_t x=0; x<8; x++) ClearDot(x,y,z);
+  }
+}
+
+
+void SetDot(int8_t x,int8_t y,int8_t z)
+{
+  //noInterrupts();
+  bitSet(cube[y][z], x);
+  //interrupts();
+}
+
+void ClearDot(int8_t x,int8_t y,int8_t z)
+{
+  bitClear(cube[y][z], x);
+}
+
+void SetLayer(int8_t z, int8_t xByte)
+{
+  //z = Wrap(z);
+  for (int8_t y=0; y<8; y++) {
+    cube[y][z] = xByte;
+    //for (int8_t x=0; x<8; x++) {
+    //  if (xByte==0) ClearDot(x,y,z); else SetDot(x,y,z);
+    //}
+  }
+}
+
+int8_t Wrap(int8_t val)
+{
+  if (val>7)
+    return 0;
+  else if (val<0)
+    return 7;
+  else
+    return val;
+}
+
+void BottomCorner()
+{
+  for (int8_t x=0; x<1; x++) {
+    for (int8_t y=7; y<8; y++) {
+      SetDot(x,y,1); 
+    }
+  }
+}
+
+void loop(void) {
+  //One_Pixel_Up_a_wall(7);
+  //Line_Up_a_wall(1);
+  //BottomUp();
+  
+  //CubeRightLeft();  //delay(1000);
+  //CubeLeftRight();  //delay(1000);
+  //CubeUp();  //delay(1000);
+  //CubeDown();  //delay(1000);
+  //CubeAllOn(); delay(2000);
+  //CubeAllOff();
+
+  //TestPattern2_Scan_one_layer(0);  //delay(1000);
+  //TestPattern_scan_all_layers();  //delay(1000);
+  //TestPattern4_Scan_one_wall();  delay(1000);
+  //TestPattern5_swipe_wall_up();  //delay(1000);
+  //CubeAllOn(); delay(1000);
+  //BottomCorner();
+  //LeftRight();
+  //OneWall();  delay(1000);
+  //LayerOn(0); delay(4000);
+
+  //-- CapSense --
+  long total1 =  cs_14_15.capacitiveSensor(30);
+  long total2 =  cs_14_16.capacitiveSensor(30);
+  long total3 =  cs_14_17.capacitiveSensor(30);
+  //Serial.print(total1);                  // print sensor output 1
+  //Serial.print("\t");
+  Serial.println(total2);
+  if (total1>200) CubeLeftRight();
+  if (total2>200) CubeUp();
+  if (total3>200) CubeRightLeft();
+  
+  pot0 = analogRead(A4);
+  pot1 = analogRead(A5);
+  
+  animSpeed = map(pot0, 0,1023, 16,256);
+  
+  CubeAllOff();
+}
