4884bd2ecc3cd58355d6e2ff823071aa25e1317f
Hari Wiguna <hwiguna@gmail.com>
2014-11-01 19:45:45 -0500

    successfully solved a few cells. About to refactor to reuse PruneCells

--- ArduinoProjects/Suduino_04/Debug.h
+++ ArduinoProjects/Suduino_04/Debug.h
@@ -1,10 +1,17 @@
 class Debug
 {
   public:
+    void DebugStr(char label[], char str[]);
     void DebugNum(char label[], unsigned int num);
     void DebugNum2(char label[], unsigned int num1, unsigned int num2);
   private:
 };
+
+void Debug::DebugStr(char label[], char str[])
+{
+  Serial.print(label);
+  Serial.println(str);
+}
 
 void Debug::DebugNum(char label[], unsigned int num)
 {
--- ArduinoProjects/Suduino_04/Scanner.h
+++ ArduinoProjects/Suduino_04/Scanner.h
@@ -8,6 +8,9 @@ class Scanner
     Board *board;
     void PruneCandidates();
     void PruneByY(byte y);
+    void PruneByX(byte x);
+    void PruneByBox(byte x, byte y);
+    void PruneCells(Cell* cells[]);
     void FindWinners();
 };
 
@@ -25,11 +28,15 @@ void Scanner::Solve()
 
 void Scanner::PruneCandidates()
 {
-  // .. Iterate through all rows and columns
   for (byte y=0; y<9; y++)
-  {
     PruneByY(y);
-  }
+
+  for (byte x=0; x<9; x++)
+    PruneByX(x);
+
+  for (byte y=0; y<3; y++)
+    for (byte x=0; x<3; x++)
+      PruneByBox(x,y);  
 }
 
 void Scanner::PruneByY(byte y)
@@ -42,16 +49,75 @@ void Scanner::PruneByY(byte y)
     if ( cell->IsSolved() ) 
     {
       byte val = cell->Get();
-      debug.DebugNum2("x,val = ", x, val);
-      // If that column has been solved, then none of the other columns in that row can be that number.
+      debug.DebugNum2("At column x found a solved value of val = ", x, val);
+      // Because this column has been solved, then none of the other columns in that row can be that number.
       for (byte x2=0; x2<9; x2++)
       {
-        if (y==3) debug.DebugNum2("Pruning value from column = ", val, x2);
         board->GetCell(x2,y)->RemoveCandidate( val );
       }
     }
   }
 }
+
+void Scanner::PruneByX(byte x)
+{
+  debug.DebugNum("Pruning column ", x);
+  // Scan every column in row y
+  for (byte y=0; y<9; y++)
+  {
+    Cell* cell = board->GetCell(x,y);
+    if ( cell->IsSolved() ) 
+    {
+      byte val = cell->Get();
+      debug.DebugNum2("At row y found a solved value of val = ", y, val);
+      // Because this column has been solved, then none of the other columns in that row can be that number.
+      for (byte y2=0; y2<9; y2++)
+      {
+        board->GetCell(x,y2)->RemoveCandidate( val );
+      }
+    }
+  }
+}
+
+void Scanner::PruneByBox(byte x0, byte y0)
+{
+  debug.DebugNum2("Pruning box x0,y0 = ", x0,y0);
+  Cell* cells[9];
+  
+  byte i = 0;
+  x0 = x0*3;
+  y0 = y0*3;
+  for (byte y=0; y<3; y++)
+  {
+    for (byte x=0; x<3; x++)
+    {
+      cells[i++] = board->GetCell(x0+x,y0+y);
+    }
+  }
+  
+  PruneCells( cells );
+}
+
+void Scanner::PruneCells(Cell* cells[])
+{
+  debug.DebugStr("PruneCells", "");
+  // Scan every column in row y
+  for (byte i=0; i<9; i++)
+  {
+    Cell* cell = cells[i];
+    if ( cell->IsSolved() ) 
+    {
+      byte val = cell->Get();
+      debug.DebugNum2("At index i found a solved value of val = ", i, val);
+      // Because this column has been solved, then none of the other columns in that row can be that number.
+      for (byte j=0; j<9; j++)
+      {
+        cells[j]->RemoveCandidate( val );
+      }
+    }
+  }
+}
+
 
 //------------------------
 
@@ -62,7 +128,7 @@ void Scanner::FindWinners()
   {
     for (byte x=0; x<9; x++)
     {
-      debug.DebugNum2("Finding Winners x,y=", x, y);
+      //debug.DebugNum2("Finding Winners x,y=", x, y);
       // If not solved, prune against numbers on that row
       board->GetCell(x,y)->FindWinner();
     }
