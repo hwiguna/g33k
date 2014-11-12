c58656ad578bf3f685b1173c3c678e178b2dc9e5
hwiguna@gmail.com <hwiguna@gmail.com>
2014-11-11 21:53:24 -0600

    Build strings and wait for sentinel values

--- ArduinoProjects/ESP8266_v01/ESP8266_v01.ino
+++ ArduinoProjects/ESP8266_v01/ESP8266_v01.ino
@@ -2,25 +2,33 @@
 //AT+CWMODE=1
 //AT+CWJAP="Firefly24","PASSWORD"
 //AT+CIPSTART="TCP","184.168.46.67",80
-//
-//  cmd = "GET /status HTTP/1.0\r\nHost: ";
-//  cmd += HOST;
-//  cmd += "\r\n\r\n";
-// 
-//  Serial.print("AT+CIPSEND=");
-//  Serial.println(cmd.length());
-  
+
+// AT+CIPSEND=70
+// 43+2+19+4
+
+// 0        1         2         3         4         5         6         7         8
+// 12345678901234567890123456789012345678901234567890123456789012345678901234567890
+// GET /data/2.5/weather?q=Lincoln,Ne HTTP/1.0
+// 0        1         2         3         4         5         6         7         8
+// 12345678901234567890123456789012345678901234567890123456789012345678901234567890
+// Host: 162.243.44.32
   
   
 #include <SoftwareSerial.h>
+#include "password.h"
 #define SSID "Firefly24"        //your wifi ssid here
  
 #define DST_IP "162.243.44.32" // "api.openweathermap.org"
 #define HOST "api.openweathermap.org"
- 
+
+//#define WIFI_BAUD  115200
+#define WIFI_BAUD  57600
+#define PC_BAUD    19200
+
 SoftwareSerial dbgSerial(10, 11); // RX, TX
  
 char myChar;
+String line;
  
 void setup()
 {
@@ -37,136 +45,67 @@ void setup()
   }
  
   // Open serial communications for WiFi module:
-  Serial.begin(115200); // firmware v0.922 defaults to 9600. use AT+CIOBAUD=115200 to change speed.
+  Serial.begin(WIFI_BAUD); // firmware v0.922 defaults to 9600. use AT+CIOBAUD=115200 to change speed.
   // Set time to wait for response strings to be found
   Serial.setTimeout(5000);
  
   //Open software serial for debugging - must use serial to usb (FTDI) adapter
   //dbgSerial.begin(19200); //can't be faster than 19200 for softserial
-  dbgSerial.begin(19200);   //38400 softserial works fine for me
-  
-//  dbgSerial.println("ESP8266 TEST");
-//  delay(1000);
-//  Serial.println("AT+RST");    
-//  delay(100);
-//  while (Serial.available()) {
-//    myChar = Serial.read();
-//    delay(25);
-//    dbgSerial.print(myChar);
-//  }
-// delay(1000);
+  dbgSerial.begin(PC_BAUD);   //38400 softserial works fine for me
       
   dbgSerial.println("ESP8266 Demo");
   delay(100);
   
-  char buffer[64];
-  char cr = 13;
-  char lf = 10;
-  int length;
-
-//  buffer[0]='H';
-//  buffer[1]='I';
-//  buffer[2]=0;
-//  dbgSerial.println(buffer);
+  //-- RESET --
+  Println("AT");
+  //delay(1000);
+  WaitFor("OK");
+  dbgSerial.println("Module is ready");
   
-  //test if the module is ready
-  Serial.println("AT+RST");    
-
-//  for (int i=0; i<5; i++)
-//  {
-//    length = 63;
-//    int actualRead = Serial.readBytesUntil(cr, buffer, length);
-//    buffer[actualRead] = 0; // Insert string terminating character so dbgSerial.print() would know when to stop printing.
-//    dbgSerial.print("line ");
-//    dbgSerial.print(i, DEC);
-//    dbgSerial.print(" actualRead=");
-//    dbgSerial.print(actualRead, DEC);
-//    dbgSerial.print(':');
-//    dbgSerial.print(buffer);
-//    dbgSerial.println('|');
-//  }
-  
+  //-- CONNECT TO WIFI --
   delay(1000);
-  if(Serial.find("ready"))
+  boolean connected=false;
+  for(int i=0;i<5;i++)
   {
-    dbgSerial.println("Module is ready");
-    delay(1000);
-    //connect to the wifi
-    boolean connected=false;
-    for(int i=0;i<5;i++)
+    if(connectWiFi())
     {
-      if(connectWiFi())
-      {
-        connected = true;
-        break;
-      }
-    }
-    if (!connected)
-    {
-      //die
-      while(1);
-    }
- 
-    delay(5000);
-    //set the single connection mode
-    Serial.println("AT+CIPMUX=0");
-  }
-  else
-  {
-    dbgSerial.println("Module didn't respond.");
-    delay(100);
-   
-    //serial loop mode for diag
-    while(1){
-      while (dbgSerial.available()) {
-        myChar = dbgSerial.read();
-        Serial.print(myChar);
-      }
- 
-      while (Serial.available()) {
-        myChar = Serial.read();
-        delay(25);
-        dbgSerial.print(myChar);
-      }
+      connected = true;
+      break;
     }
   }
-}
+  if (!connected)
+  {
+    //die
+    while(1);
+  }
  
-void loop()
-{
+  delay(5000);
+
+  //-- SINGLE CONNECTION MODE --
+  Println("AT+CIPMUX=0");
+  WaitFor("OK");
+  
+  //-- CONNECT TO A WEBSITE --
   String cmd = "AT+CIPSTART=\"TCP\",\"";
   cmd += DST_IP;
   cmd += "\",80";
-  Serial.println(cmd);
-  dbgSerial.println(cmd);
-  if(Serial.find("Error")) return;
-//  cmd = "GET /status HTTP/1.0\r\nHost: ";
-//  cmd += HOST;
-//  cmd += "\r\n\r\n";
+  Println(cmd);
+  WaitFor("OK");
 
-  cmd = "GET /data/2.5/weather?q=Lincoln,Ne&units=imperial HTTP/1.0\r\nHost: ";
+  //-- ASK FOR A WEB PAGE --
+  cmd = "GET /data/2.5/weather?q=Lincoln,Ne HTTP/1.1\r\nHost: ";
   cmd += HOST;
-  cmd += "\r\n\r\n";
+  cmd += "\r\n";
  
-  Serial.print("AT+CIPSEND=");
-  Serial.println(cmd.length());
-  if(Serial.find(">"))
-  {
-    dbgSerial.print(">");
-  }
-  else
-  {
-    Serial.println("AT+CIPCLOSE");
-    dbgSerial.println("connect timeout");
-    delay(1000);
-    return;
-  }
+  String len = String( cmd.length() );
+  Println("AT+CIPSEND=" + len);
+  WaitFor("> ");
+  
+  Print(cmd);
  
-  Serial.print(cmd);
- 
-  delay(500);
+  delay(2000);
   //Serial.find("+IPD");
-  while (Serial.available())
+  while (true) //Serial.available())
   {
    char c = Serial.read();
    dbgSerial.write(c);
@@ -176,29 +115,66 @@ void loop()
  
  dbgSerial.println();
  dbgSerial.println("====");
- delay(1000);
+ delay(5000);
+}
+ 
+void loop()
+{
+
 }
  
  
 boolean connectWiFi()
 {
-  Serial.println("AT+CWMODE=1");
+  Println("AT+CWMODE=1");
   String cmd="AT+CWJAP=\"";
   cmd+=SSID;
   cmd+="\",\"";
   cmd+=PASS;
   cmd+="\"";
-  dbgSerial.println(cmd);
-  Serial.println(cmd);
-  delay(2000);
-  if(Serial.find("OK"))
-  {
-    dbgSerial.println("OK, Connected to WiFi.");
-    return true;
-  }
-  else
-  {
-    dbgSerial.println("Can not connect to the WiFi.");
-    return false;
-  }            
+  
+  Println(cmd);
+  WaitFor("OK");
+
+  dbgSerial.println("OK, Connected to WiFi.");
+  return true;
+}
+
+void Print(String str)
+{
+  dbgSerial.print("PRINT='");
+  dbgSerial.print(str);
+  dbgSerial.println("'");
+
+  Serial.print(str);
+}
+
+void Println(String str)
+{
+  dbgSerial.print("PRINTLINE='");
+  dbgSerial.print(str);
+  dbgSerial.println("'");
+
+  Serial.println(str);
+}
+
+String ReadLine()
+{
+  String line = "";
+  
+  line = Serial.readStringUntil(13);
+  Serial.readStringUntil(10);
+  
+  dbgSerial.print("LINE='");
+  dbgSerial.print(line);
+  dbgSerial.println("'");
+  
+  return line;
+}
+
+void WaitFor(String whatToWaitFor)
+{
+  String line;
+  while (line != whatToWaitFor)
+    line = ReadLine();
 }
