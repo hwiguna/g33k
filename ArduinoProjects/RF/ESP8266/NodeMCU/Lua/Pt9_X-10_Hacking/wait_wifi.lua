2c54f7b2e9e7c89e5c5a914de9667bda35be130c
hwiguna@gmail.com <hwiguna@gmail.com>
2014-12-15 13:11:23 -0600

    $3 Wifi module part 9: X-10 Hacking

---
+++ ArduinoProjects/RF/ESP8266/NodeMCU/Lua/Pt9_X-10_Hacking/wait_wifi.lua
@@ -0,0 +1,19 @@
+-- from http://www.esp8266.com/viewtopic.php?f=18&t=628
+function wait_wifi()
+  count = count + 1
+  wifi_ip = wifi.sta.getip()
+  if wifi_ip == "0.0.0.0" and count < 20 then
+    tmr.alarm(0, 1000,0,wait_wifi)
+  elseif count >= 20 then
+    wifi_connected = false
+    print("Wifi connect timed out.")
+  else
+    wifi_connected = true
+    print("Got IP "..wifi_ip.."\n")
+    dofile("x10_v02.lua")
+  end
+end
+
+-- Wait for WiFi --
+count=0
+wait_wifi()
