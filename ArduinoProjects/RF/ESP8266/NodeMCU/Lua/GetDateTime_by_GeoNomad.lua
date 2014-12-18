1135ae54998971a1f8f2419a409af897da51e32c
hwiguna@gmail.com <hwiguna@gmail.com>
2014-12-18 00:42:53 -0600

    Cool trick

---
+++ ArduinoProjects/RF/ESP8266/NodeMCU/Lua/GetDateTime_by_GeoNomad.lua
@@ -0,0 +1,13 @@
+
+conn=net.createConnection(net.TCP, 0) 
+conn:on("receive", function(conn, payload)
+    print(string.sub(payload,string.find(payload,"Date: ")+6,string.find(payload,"Date: ")+35))
+    conn:close()
+    end) 
+conn:dns('google.com',function(conn,ip) ipaddr=ip end)
+conn:connect(80,ipaddr) 
+heap = node.heap()
+conn:send("HEAD / HTTP/1.1\r\n") 
+conn:send("Accept: */*\r\n") 
+conn:send("User-Agent: Mozilla/4.0 (compatible; ESP8266 NodeMcu Lua;)\r\n") 
+conn:send("\r\n") 
