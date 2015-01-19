d9d6f4da3f7392ec41691221e3ba18d4293e4070
hwiguna@gmail.com <hwiguna@gmail.com>
2015-01-18 19:36:32 -0600

    sync

--- ArduinoProjects/RF/ESP8266/NodeMCU/Lua/WorldWideESP/WorldWideTX.lua
+++ ArduinoProjects/RF/ESP8266/NodeMCU/Lua/WorldWideESP/WorldWideTX.lua
@@ -1,19 +1,16 @@
--- Setup feedback LED --
-beepPin = 4 -- GPIO 02
-gpio.mode(beepPin, gpio.OUTPUT)
+led=4 -- GPIO02
+gpio.mode(led,gpio.OUTPUT)
 
--- Beep Transmitter --
 function Transmit()
-  gpio.write(beepPin, 0 ) -- ON
+  gpio.write(led, 0 ) -- ON
   conn = net.createConnection( net.TCP, false)
   conn:on("receive", function(c, payload) 
     print(payload)
-    gpio.write(beepPin, 1 ) -- off
+    gpio.write(led, 1 ) -- off
   end)
   conn:connect(80,"192.168.254.100")
   conn:send("GET /bip1 HTTP/1.1\r\n\r\n")
 end
-
+print("Ready to Transmit!")
 Transmit()
 tmr.alarm(0, 2000, 1, Transmit)
-
