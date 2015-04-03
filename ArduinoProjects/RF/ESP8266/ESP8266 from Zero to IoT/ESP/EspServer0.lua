bde90960c8cf7a07065f4b5de502d54ecda73fc8
hwiguna@gmail.com <hwiguna@gmail.com>
2015-04-02 22:35:57 -0500

    Comment out prints because they seem to slow down the response time.

--- ArduinoProjects/RF/ESP8266/ESP8266 from Zero to IoT/ESP/EspServer0.lua
+++ ArduinoProjects/RF/ESP8266/ESP8266 from Zero to IoT/ESP/EspServer0.lua
@@ -23,18 +23,18 @@ function EspServer()
 	srv:listen(serverPort,function(conn)
 
 	  conn:on("receive",function(conn,payload)
-		print(node.heap())
-		print(payload)
+		--print(node.heap())
+		--print(payload)
 		
 		-- 123456789012
 		-- GET /set?v=1 HTTP/1.1 --
 		command = string.sub(payload, 6,8) -- Get characters 6,7, and 8
 
 		if command == "set" then
-			sentValue = string.sub(payload, 12,12) -- Get the 12th character
-			print("sent value=" .. sentValue)
-			gpio.write(ledPin, sentValue)
-			reply = "{ \"led\":\"" .. tostring(sentValue) .. "\" }"
+			receivedValue = string.sub(payload, 12,12) -- Get the 12th character
+			print("Received value=" .. receivedValue)
+			gpio.write(ledPin, receivedValue)
+			reply = "{ \"led\":\"" .. tostring(receivedValue) .. "\" }"
 			status = "HTTP/1.1 200 OK\r\n"
 		elseif command == "get" then
 			switchValue = gpio.read( switchPin )
