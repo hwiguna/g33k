d54cc58f95696e019c863fa89f6bacdf1f80f25a
hwiguna <hwiguna@gmail.com>
2015-08-24 10:15:50 -0500

    The PC and Arduino pair now works. Still need tons of error handling.

--- ArduinoProjects/2015/_Learning/OcuveraAlert01/OcuveraAlert01.ino
+++ ArduinoProjects/2015/_Learning/OcuveraAlert01/OcuveraAlert01.ino
@@ -7,33 +7,33 @@ void setup()
   pinMode(2,INPUT); // Set to input so we can float it
   digitalWrite(2,LOW); // no pullup resistor = float this pin
   
-  // start serial port at 9600 bps and wait for port to open:
-  Serial.begin(9600);
+  // start serial port at 115200 bps and wait for port to open:
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
-  establishContact();  // send a byte to establish contact until receiver responds
+  //establishContact();  // send a byte to establish contact until receiver responds
 }
 
 void loop()
 {
   // if we get a valid byte, read analog ins:
   if (Serial.available() > 0) {
-    // get incoming byte:
-    inByte = Serial.read();
-    
-    Serial.print("Sending!");
+    // get incoming string (don't care what it says)
+    String ignored = Serial.readString();
     
     pinMode(2,OUTPUT); // switching pin 2 to output will show its state (LOW)
     delay(100);
     pinMode(2,INPUT); // switching pin 2 to input will return pin to float
+    
+    Serial.print("OK");
   }
 }
 
 void establishContact() {
   while (Serial.available() <= 0) {
     Serial.println("Press any key...");   // send an initial string
-    delay(1000);
+    delay(500);
   }
 }
---
+++ ArduinoProjects/Windows/Misc/OcuveraAlertTest/OcuveraAlertTest/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OcuveraAlertTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SerialPort _serialPort;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            string[] portNames = SerialPort.GetPortNames();
+            _serialPort = new SerialPort(portNames[0], 115200);
+            _serialPort.DataReceived += (o, args) => StatusTextblock.Dispatcher.Invoke(new System.Action(() =>serialPort_DataReceived(o,args)));
+            _serialPort.Open();
+            StatusTextblock.Text = "PC Ready!";
+        }
+
+        private void TestButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            string msg = "Go!";
+            _serialPort.WriteLine(msg); // Arduino does not care what we send.  Any string will cause it to pulse pin 2
+        }
+
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort) sender;
+            StatusTextblock.Text = DateTime.Now.ToLongTimeString() + " " + sp.ReadExisting();
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            if (_serialPort != null && _serialPort.IsOpen) _serialPort.Close();
+        }
+    }
+}
